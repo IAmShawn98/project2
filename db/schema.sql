6b3bea9c4f08c8a38148096c6516d0629a51f7b7
conniepope <conniepope77@gmail.com>
2019-08-06 16:56:32 -0400

    schema and seeds sql files with tables

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE timesheet_db;
+
+USE timesheet_db;
+
+CREATE TABLE employees (
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    team_member VARCHAR (100) NOT NULL,
+    title VARCHAR (200),
+    tier_level VARCHAR (30),
+    hours_used INT (10),
+    hours_remaining INT (10),
+    start_date DATE,
+    admin BOOLEAN DEFAULT false
+    );
+    
+CREATE TABLE login (
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    team_member VARCHAR (100) NOT NULL,
+    login VARCHAR (100) NOT NULL,
+    password VARCHAR (100) NOT NULL
+    );
+    
+CREATE TABLE tiers (
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    blue INT (10),
+    green INT (10),
+    purple INT (10)
+    );
+       
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO employees (team_member, title, tier_level, hours_used, hours_remaining, start_date, admin) VALUES
+    ("Billy Bob", "Accounting Associate", "blue", 22, 58, "2018-05-05", 0),
+    ("Lilly Bob", "Accounting Manager", "green", 0, 120, "2012-05-05", 0),
+    ("Philly Bob", "Senior Executive CEO", "purple", 0, 200, "1998-12-12", 1);
+
+INSERT INTO login (team_member, login, password) VALUES
+    ("Billy Bob", "billybob@mail.com", "abcdefg123"),
+    ("Lilly Bob", "lillybob@mail.com", "thisisnotmypassword"),
+    ("Philly Bob", "phillybob@mail.com", "ilikecheesesteaks");
+
+INSERT INTO tiers (blue, green, purple) VALUES
+    (80, 120, 200);
