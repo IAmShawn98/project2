045b5990639e40053082e0b144e8461fd0627b69
conniepope <conniepope77@gmail.com>
2019-08-09 14:15:04 -0400

    fixed schema to accomodate sequelize requirements

--- db/schema.sql
+++ db/schema.sql
@@ -11,20 +11,26 @@ CREATE TABLE employees (
     hours_remaining INT (10),
     start_date DATE,
     end_date DATE,
-    admin BOOLEAN DEFAULT false
+    admin BOOLEAN DEFAULT false,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
     );
     
 CREATE TABLE login (
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     team_member VARCHAR (100) NOT NULL,
     username VARCHAR (100) NOT NULL,
-    password VARCHAR (100) NOT NULL
+    password VARCHAR (100) NOT NULL,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
     );
     
 CREATE TABLE tiers (
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     blue INT (10),
     green INT (10),
-    purple INT (10)
+    purple INT (10),    
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
     );
        
